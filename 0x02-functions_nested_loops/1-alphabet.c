24392740b7f8bb4d1feb143a74df977db01205f4
CyberMMA <cybermma0@gmail.com>
2023-07-20 01:10:43 +0100

    motd

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry point
+ * Return:0
+ *(c) - cybermma
+ */
+
+void print_alphabet(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+
+	}
+	_putchar('\n');
+
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+#learning nested loops and functions from alx
+more low level lang from alx
